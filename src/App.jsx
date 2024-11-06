0aaea990996f460bab171912f353cb207422d4b6
Murshyne <murshyne@gmail.com>
2024-11-05 20:28:17 -0500

    Committed at: Tue, Nov  5, 2024  8:28:16 PM

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,32 @@ import { useState, useEffect } from "react";
 
 import "./App.css";
 
-// Import our components
 import MovieDisplay from "./components/MovieDisplay";
 import Form from "./components/Form";
 
 export default function App() {
+  // Constant with your API Key
+  const apiKey = "98e3fb1f";
+
+  // State to hold movie data
+  const [movie, setMovie] = useState(null);
+
+  // Function to get movies
+  const getMovie = async (searchTerm) => {
+    // Make fetch request and store the response
+    const response = await fetch(
+      `http://www.omdbapi.com/?apikey=${apiKey}&t=${searchTerm}`
+    );
+    // Parse JSON response into a JavaScript object
+    const data = await response.json();
+    // Set the Movie state to the received data
+    setMovie(data);
+  };
+
+  // We pass the getMovie function as a prop called moviesearch
   return (
     <div className="App">
-      <Form />
+      <Form moviesearch={getMovie} />
       <MovieDisplay />
     </div>
   );
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,4 +1,10 @@
 export default function Form(props) {
-  // The component must return some JSX
-  return <h1>The Form Component</h1>;
+  return (
+    <div>
+      <form>
+        <input type="text" />
+        <input type="submit" value="submit" />
+      </form>
+    </div>
+  );
 }
